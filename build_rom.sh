66e64b54eb76ef1be41e3b928fd8e0e43b5a8a9a
Dark angels <95138156+darkangels15@users.noreply.github.com>
2023-07-31 05:12:05 +0700

    build_rom.sh

---
+++ build_rom.sh
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+    <remote name="git" fetch="https://github.com" />
+    <remote name="glab" fetch="https://gitlab.com" />
+    
+    <project
+        name="Jasmine-Trees/android_device_xiaomi_jasmine_sprout"
+        path="device/xiaomi/jasmine_sprout"
+        remote="git"
+        revision="cherish"
+    />
+    <project
+        name="Jasmine-Trees/android_device_xiaomi_sdm660-common"
+        path="device/xiaomi/sdm660-common"
+        remote="git"
+        revision="13"
+    />
+    <project
+        name="Jasmine-Trees/android_kernel_xiaomi_scarlet"
+        path="kernel/xiaomi/sdm660"
+        remote="git"
+        revision="qti-haptics"
+    />
+    <!-- Removed projects -->
+    <remove-project name="android_device_qcom_sepolicy" />
+    <remove-project name="ArrowOS/android_hardware_qcom_audio" />
+    <remove-project name="ArrowOS/android_hardware_qcom_media" />
+    <remove-project name="ArrowOS/android_hardware_qcom_display" />
+
+    <!-- Misc projects -->
+    <project
+        name="Jasmine-Trees/platform_hardware_qcom-caf_sdm660_media"
+        path="hardware/qcom-caf/sdm660/media"
+        remote="git"
+        revision="tortilla"
+    />
+    <project
+        name="Jasmine-Trees/platform_hardware_qcom-caf_sdm660_audio"
+        path="hardware/qcom-caf/sdm660/audio"
+        remote="git"
+        revision="tortilla"
+    />
+    <project
+        name="Jasmine-Trees/platform_hardware_qcom-caf_sdm660_display"
+        path="hardware/qcom-caf/sdm660/display"
+        remote="git"
+        revision="tortilla"
+    />
+    <project
+        name="Jasmine-Trees/device_qcom_sepolicy-legacy-um"
+        path="device/qcom/sepolicy-legacy-um"
+        remote="git"
+        revision="tiramisu"
+    />
+</manifest>
